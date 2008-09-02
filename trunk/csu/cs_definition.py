4d157999abc0f1e24a4bc053d44e51680db470a0
morphinewan <morphinewan@fba9e046-6655-0410-a53f-e708bdfe6085>
2008-09-02 01:27:47 +0000

    整理定义

---
+++ trunk/csu/cs_definition.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import wx
+#系统设置
+Application_Settings = {}
+
+#菜单ID
+ID_MenuItem_OpenFile = wx.NewId()
+ID_MenuItem_Exit = wx.NewId()
+ID_MenuItem_About = wx.NewId()
+ID_MenuItem_ShowOptionPanel = wx.NewId()
+
+#工具栏
+ID_ToolBar_ShowOptionPanel = wx.NewId()
+ID_ToolBar_ZoomImage = wx.NewId()
+ID_ToolBar_OpenFile = wx.NewId()
+
+#参数区
+ID_Panel_Option = wx.NewId()
+ID_Option_PrintScale = wx.NewId()
+ID_Option_PreviewScale = wx.NewId()
+ID_Option_BgColour = wx.NewId()
+ID_Option_MaxColourNum = wx.NewId()
+ID_Option_MinFlossNum = wx.NewId()
+ID_Option_MixColourDist = wx.NewId()
+ID_Option_Width = wx.NewId()
+ID_Option_Height = wx.NewId()
+ID_Option_CT = wx.NewId()
+ID_Option_CropSide = wx.NewId()
+ID_Option_AntiNoise = wx.NewId()
+ID_Option_AntiBgColour = wx.NewId()
+ID_Option_OnlyPreview = wx.NewId()
+ID_Option_ForTaobao = wx.NewId()
+ID_Option_DisabledBgColour = wx.NewId()
+
+#图片区ID
+ID_Panel_Work = wx.NewId()
+ID_Panel_Work_ImageReview = wx.NewId()
+
+#Log区
+ID_Panel_Log = wx.NewId()
+
